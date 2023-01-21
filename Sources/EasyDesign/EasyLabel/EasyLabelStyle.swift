5e7aa7b4afc2f5391a0a8bd9df31e1fbeffe50d9
Daniil Shchepkin <dan.shepkin@gmail.com>
2023-01-21 17:40:18 +0300

    EasyLabel view has been added

---
+++ Sources/EasyDesign/EasyLabel/EasyLabelStyle.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLabelStyle.swift
+//
+//  Struct containing style properties for the EasyLabel class
+//
+//  Created by Daniil Shchepkin on 21.01.2023.
+//
+
+import UIKit
+
+public struct EasyLabelStyle {
+    public let font: UIFont? = nil
+    public let fontSize: CGFloat?  = nil
+    
+    public let textColor: UIColor? = nil
+    public let textAlignment: NSTextAlignment? = nil
+}
