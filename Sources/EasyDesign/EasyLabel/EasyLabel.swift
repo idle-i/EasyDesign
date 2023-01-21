d4652a4bd07f7f34eefaed886eae7b26500790e5
Daniil Shchepkin <61019991+idle-i@users.noreply.github.com>
2023-01-21 18:49:49 +0300

    v 0.1
    - EasyLabel view has been added
    - Added EasyLabelStyle constructor, fixed EasyLabel constructors
    - README has been added

---
+++ Sources/EasyDesign/EasyLabel/EasyLabel.swift
@@ -0,0 +1,92 @@
+//
+//  EasyLabel.swift
+//
+//  A class that represents a custom implementation of the UILabel class from UIKit
+//
+//  Created by Daniil Shchepkin on 21.01.2023.
+//
+
+import UIKit
+
+@IBDesignable
+public class EasyLabel: UILabel {
+
+    // MARK: - Private Variables
+    
+    private var clickHandler: (() -> Void)? {
+        didSet { self.updateGestureRecognizer() }
+    }
+    
+    private var tapGestureRecognizer: UITapGestureRecognizer?
+    
+    // MARK: - Initializers
+    
+    public convenience init(text: String) {
+        self.init(frame: .zero)
+        
+        self.text = text
+    }
+    
+    public convenience init(style: EasyLabelStyle) {
+        self.init(frame: .zero)
+        
+        self.viewDidSetStyle(style)
+    }
+    
+    // MARK: - Public Methods
+    
+    public func updateStyle(_ style: EasyLabelStyle) {
+        viewDidSetStyle(style)
+    }
+    
+    public func setClickHandler(_ clickHandler: @escaping () -> Void) {
+        self.clickHandler = clickHandler
+    }
+    
+    public func removeClickHandler() {
+        self.clickHandler = nil
+    }
+    
+    // MARK: - Private Methods
+    
+    private func viewDidSetStyle(_ style: EasyLabelStyle) {
+        if let font = style.font, self.font != font {
+            self.font = font
+        }
+        
+        if let fontSize = style.fontSize {
+            self.font = self.font.withSize(fontSize)
+        }
+        
+        if let textColor = style.textColor, self.textColor != textColor {
+            self.textColor = textColor
+        }
+        
+        if let textAlignment = style.textAlignment, self.textAlignment != textAlignment {
+            self.textAlignment = textAlignment
+        }
+    }
+    
+    private func updateGestureRecognizer() {
+        isUserInteractionEnabled = self.clickHandler != nil
+        
+        if self.clickHandler != nil {
+            self.tapGestureRecognizer = UITapGestureRecognizer(
+                target: self,
+                action: #selector(self.tapActionHandle)
+            )
+            
+            addGestureRecognizer(self.tapGestureRecognizer!)
+        } else {
+            if let tapGestureRecognizer = self.tapGestureRecognizer {
+                removeGestureRecognizer(tapGestureRecognizer)
+            }
+            
+            self.tapGestureRecognizer = nil
+        }
+    }
+    
+    // MARK: - Callbacks
+    
+    @objc private func tapActionHandle() { self.clickHandler?() }
+}
---
+++ Sources/EasyDesign/EasyLabel/EasyLabelStyle.swift
@@ -0,0 +1,29 @@
+//
+//  EasyLabelStyle.swift
+//
+//  Struct containing style properties for the EasyLabel class
+//
+//  Created by Daniil Shchepkin on 21.01.2023.
+//
+
+import UIKit
+
+public struct EasyLabelStyle {
+    let font: UIFont?
+    let fontSize: CGFloat?
+    
+    let textColor: UIColor?
+    let textAlignment: NSTextAlignment?
+    
+    public init(
+        font: UIFont? = nil,
+        fontSize: CGFloat? = nil,
+        textColor: UIColor? = nil,
+        textAlignment: NSTextAlignment? = nil
+    ) {
+        self.font = font
+        self.fontSize = fontSize
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+    }
+}
