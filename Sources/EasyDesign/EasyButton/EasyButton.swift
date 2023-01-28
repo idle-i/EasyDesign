95c53ebf650c3d668ae8d8e7ffef9788cd9b5f39
Daniil Shchepkin <dan.shepkin@gmail.com>
2023-01-28 16:00:37 +0300

    Added vibration feedback for press event

--- Sources/EasyDesign/EasyButton/EasyButton.swift
+++ Sources/EasyDesign/EasyButton/EasyButton.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+public enum EasyButtonVibrationIntensity {
+    case light
+    case medium
+    case heavy
+}
+
 @IBDesignable
 public class EasyButton: UIButton, EasyView {
     
@@ -28,6 +34,9 @@ public class EasyButton: UIButton, EasyView {
     public var isZoomEnabled: Bool = false
     public var zoomFactor: CGFloat = 1.0
     public var zoomDuration: Double = 0.3
+    
+    public var isVibrationEnabled: Bool = false
+    public var vibrationIntensity: EasyButtonVibrationIntensity = .light
     
     // MARK: - Private Variables
     
@@ -144,19 +153,35 @@ public class EasyButton: UIButton, EasyView {
     }
     
     private func viewDidHighlight() {
-        if self.isZoomEnabled {
-            UIView.animate(withDuration: self.zoomDuration) { [weak self] in
-                guard let self = self else { return }
-                
-                self.transform = CGAffineTransform(
-                    scaleX: self.isHighlighted ? self.zoomFactor : 1.0,
-                    y: self.isHighlighted ? self.zoomFactor : 1.0
-                )
-            }
+        if self.isZoomEnabled { self.zoomAnimation() }
+    }
+    
+    private func zoomAnimation() {
+        UIView.animate(withDuration: self.zoomDuration) { [weak self] in
+            guard let self = self else { return }
+            
+            self.transform = CGAffineTransform(
+                scaleX: self.isHighlighted ? self.zoomFactor : 1.0,
+                y: self.isHighlighted ? self.zoomFactor : 1.0
+            )
         }
+    }
+    
+    private func performVibration() {
+        UIImpactFeedbackGenerator(
+            style:
+                (self.vibrationIntensity == .light) ? .light :
+                (self.vibrationIntensity == .medium) ? .medium :
+                .heavy
+        ).impactOccurred()
     }
     
     // MARK: - Callbacks
     
-    @objc private func tapActionHandle() { self.clickHandler?() }
+    @objc private func tapActionHandle() {
+        if self.isVibrationEnabled { self.performVibration() }
+        
+        self.clickHandler?()
+    }
 }
+
